a9b8bd408693d7823ae7d9ba1199c036b139f432
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-04-24 01:27:22 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,20 +6,20 @@ import moment from 'moment-timezone'
 
 global.botNumber = '' 
 global.owner = [
-  ['5492644996684', '𝕮𝖍𝖎𝖓𝖆 💋', true],
-  ['923297474483', '𝕮𝖍𝖎𝖓𝖆 💋', true],
-  ['5492645765402','𝕮𝖍𝖎𝖓𝖆 💋',true]
+  ['5492644996684', '𝐁𝐞𝐧𝐣𝐚', true],
+  ['923297474483', '𝐁𝐞𝐧𝐣𝐚', true],
+  ['5492645765402','𝐁𝐞𝐧𝐣𝐚',true]
 ]
 global.mods = []
 global.prems = []
 global.suittag = ['923297474483']
 
-global.botname = 'Nyxira-bot'
+global.botname = '𝐍ᥡ᥊іrᥲ'
 global.vs = '1.0.0 (Lite) '
-global.author = '@its.chinitaaa_'
+global.author = '@_.𝗯𝗲𝗻𝗷𝗮𝘅𝘇𝘇'
 global.sticker = 's𝗍іᥴkᥱr ᥴrᥱᥲ𝗍ᥱძ ᑲᥡ Nyxira'
 
-global.banner = 'https://files.catbox.moe/9rte27.jpg'
+global.banner = 'https://files.catbox.moe/vpv1j2.png'
 global.icono = ''
 global.logo = ''
 
@@ -30,7 +30,7 @@ global.multiplier = 60
 global.prefix = /^[./!#?]/
 
 global.channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i'
-global.md = 'https://github.com/itsmiaa-official/Nezuko'
+global.md = 'https://github.com/benjaxzz/Nyxira'
 global.ch = { id: '120363345778623279@newsletter' }
 
 let file = fileURLToPath(import.meta.url)
